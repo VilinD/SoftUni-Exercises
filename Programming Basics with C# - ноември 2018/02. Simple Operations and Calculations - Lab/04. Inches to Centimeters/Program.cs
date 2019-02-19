4f35011df17657cffb1da725ecc1df0e68d4a552
Vilin Dimitrov <vilindimitrov99@gmail.com>
2019-02-19 08:07:00 +0200

    Adding solutions for Programming Basics

---
+++ Programming Basics with C# - ноември 2018/02. Simple Operations and Calculations - Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+
+            double convertedToCm = inches * 2.54;
+
+            Console.WriteLine($"{convertedToCm:f2}");
+        }
+    }
+}
---
+++ Programming Basics with C# - ноември 2018/03. Simple Operations and Calculations - Exercise/04. Tailoring Workshop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Tailoring_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfTables = int.Parse(Console.ReadLine());
+            double lengthOfTables = double.Parse(Console.ReadLine());
+            double widthOfTables = double.Parse(Console.ReadLine());
+
+            double totalTableclothArea = countOfTables *
+                ((lengthOfTables + 2 * 0.30) *
+                (widthOfTables + 2 * 0.30));
+     
+            double squaresArea = countOfTables * ((lengthOfTables / 2) * (lengthOfTables / 2));
+
+            double priceInDollars = totalTableclothArea * 7 + squaresArea * 9;
+            double priceInLev = priceInDollars * 1.85;
+
+            Console.WriteLine($"{priceInDollars:F2} USD");
+            Console.WriteLine($"{priceInLev:F2} BGN");
+        }
+    }
+}
